c5e5516d6f37dd2b4e570514e72beb92df20c731
Diego Bueno <diegobueno.dev@gmail.com>
2025-11-07 16:10:50 -0500

    Adding Apollo Server and GraphQL playground

---
+++ src/resolvers/_QueryResolver.js
@@ -0,0 +1,5 @@
+export const _QueryResolver = {
+    Query: {
+        greeting: () => "Hello Abestado",
+    },
+};
--- src/server.js
+++ src/server.js
@@ -1,13 +1,33 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware as apolloMiddleware } from "@as-integrations/express4";
 import cors from "cors";
 import express from "express";
+import { readFile } from "node:fs/promises";
 import { authMiddleware, handleLogin } from "./auth/auth.js";
+import { _QueryResolver } from "./resolvers/_QueryResolver.js";
 
 const app = express();
 app.use(cors(), express.json(), authMiddleware);
 
 app.post("/login", handleLogin);
 
-const PORT = 9000;
+const typeDefs = await readFile("./schema.graphql", "utf8");
+const resolvers = {
+    ..._QueryResolver,
+};
+
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    playground: true,
+});
+// apolloServer.applyMiddleware({ app });
+
+await apolloServer.start();
+app.use("/graphql", apolloMiddleware(apolloServer));
+
+const PORT = 3001;
 app.listen({ port: PORT }, () => {
     console.log(`Server running on port ${PORT}`);
+    console.log(`GraphQL endpoint: http://localhost:${PORT}/graphql`);
 });
